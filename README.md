14a80e2c27e27d9acc2c90a691f73e0a520f780b
Chen Bin <chenbin.sh@gmail.com>
2019-08-21 17:12:45 +1000

    first import

---
+++ README.md
@@ -0,0 +1,69 @@
+# [matchit](https://github.com/redguardtoo/vscode-matchit)
+Jump between matching HTML tags and brackets smartly in VS Code. It's ported from [Vim matchit](http://www.vim.org/scripts/script.php?script_id=39) by Benji Fisher.
+
+The extension provides the command `MatchIt: Jump Items` to jump between HTML tags or brackets to help Javascript developer using JSX and React.
+
+It's generic enough to support HTML/CSS/C/C++/Java/Perl/Javascript.
+
+![Demo](https://raw.githubusercontent.com/redguardtoo/vscode-matchit/master/demo.gif)
+
+## Features
+Press `Ctrl+Shift+\` (on Mac it's `CMD+Shift+\`) or run command `MatchIt: Jump Items` to jump between HTML tags or brackets
+
+Run command `MatchIt: Select Items` could select area marked by matching tags or brackets.
+
+Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+
+For example, in below code, if cursor is at the beginning or end of class, function, conditional statement, html tag, you can use all commands from this extension.
+
+Please note the cursor is not required exactly over the bracket character. The matching algorithm is smart enough to find correct matching item automatically.
+```jsx
+export default class DropdownWithFilter extends PureComponent {
+  render() {
+    const {
+      id,
+      className,
+      style,
+      ...props
+    } = this.props;
+    if(!id) {
+      return null;
+    }
+    return (
+      <select
+        id={id}
+        className={className}
+        style={style}
+      >
+        <option value="1">1</option>
+        <option value="1">1</option>
+      </select>
+    );
+  }
+}
+```
+
+## Custom Keybinding
+You can also set custom shortcuts in `keybindings.json` via `Code => Preferences => Keyboard Shortcuts`.
+
+```javascript
+[
+  {
+    "key": "ctrl+shift+\\",
+    "command": "extension.matchitJumpItems",
+    "when": "editorTextFocus"
+  },
+  {
+    "key": "ctrl+shift+|",
+    "command": "extension.matchitSelectItems",
+    "when": "editorTextFocus"
+  },
+]
+```
+
+## Issues
+Please submit issues to [vscode-matchit support](https://github.com/redguardtoo/vscode-matchit)
+
+## License 
+
+This extension is released under [the GPL v3.0 license](https://raw.githubusercontent.com/redguardtoo/vscode-matchit/master/LICENSE).
---
+++ src/extension.ts
@@ -0,0 +1,75 @@
+import * as vscode from 'vscode';
+import { moveCursor, cursorTo } from 'readline';
+import { start } from 'repl';
+
+function jumpToMatchingTag(editor:vscode.TextEditor, position:vscode.Position, currentLineNum:number) {
+  const line = editor.document.lineAt(currentLineNum).text; // get current line text
+  const followingChar = editor.document.getText(new vscode.Range(position, new vscode.Position(currentLineNum, position.character + 1)));
+  if('{}[]()'.indexOf(followingChar) >=0) {
+    vscode.commands.executeCommand('editor.action.jumpToBracket');
+  } else if(line.match(/^[ \t]*(<[A-Za-z]|<\/[A-Za-z][a-zA-Z0-9]*)/)) {
+    vscode.commands.executeCommand('editor.emmet.action.matchTag');
+  } else if(line.match(/^[ \t]*[A-Za-z].*[ \t]*[\{\(\[]$/)) {
+    const lineEndPos = new vscode.Position(currentLineNum, line.length - 1);
+    if(editor.selection.isEmpty) {
+      editor.selection = new vscode.Selection(lineEndPos, lineEndPos);
+    } else {
+      editor.selection = new vscode.Selection(editor.selection.anchor, lineEndPos);
+    }
+    vscode.commands.executeCommand('editor.action.jumpToBracket');
+  } else {
+    vscode.commands.executeCommand('editor.action.jumpToBracket');
+  }
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "matchit" is now active!');
+
+  let disposableJumpItems = vscode.commands.registerCommand('extension.matchitJumpItems', () => {
+    // Only useful when coding in a text editor
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
+    const startPosition =  editor.selection.active;
+    const currentLineNum = startPosition.line;
+    jumpToMatchingTag(editor, startPosition, currentLineNum);
+  });
+
+  let disposableSelectItems = vscode.commands.registerCommand('extension.matchitSelectItems', () => {
+    // Only useful when coding in a text editor
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
+    const startPosition =  editor.selection.active;
+    const currentLineNum = startPosition.line;
+    jumpToMatchingTag(editor, startPosition, currentLineNum);
+    setTimeout(() => {
+      const endPosition = editor.selection.active;
+      // same line
+      if(startPosition.line === endPosition.line) {
+        editor.selection = new vscode.Selection(startPosition, endPosition);
+      } else {
+        // try to select whole line
+        const topDown = startPosition.line < endPosition.line;
+        const anchor = new vscode.Position(startPosition.line, topDown ? 0 : editor.document.lineAt(startPosition.line).text.length + 2);
+        const active = new vscode.Position(endPosition.line, topDown ? editor.document.lineAt(endPosition.line).text.length + 2 : 0);
+        editor.selection = new vscode.Selection(anchor, active);
+      }
+    }, 200)
+  });
+
+  context.subscriptions.push(disposableJumpItems);
+  context.subscriptions.push(disposableSelectItems);
+}
+
+export function deactivate() {}
+
+// Local Variables:
+// coding: utf-8
+// tab-width: 2
+// js-indent-level: 2
+// typescript-indent-level: 2
+// End:
+// vim: set fs=javascript fenc=utf-8 et ts=2 sts=2 sw=2
